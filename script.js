1145f47e923e60f51975f4f0eb7ae741941d221b
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-31 15:44:29 -0300

    31/08/2023_01

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('newsletter-form');
-  
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-  
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-  
-      window.location.href = `mailto:3pmdigital.adm@gmail.com?subject=Inscrição para Newsletter&body=Nome: ${name}%0D%0AEmail: ${email}`;
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('newsletter-form');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    window.location.href = `mailto:3pmdigital.adm@gmail.com?subject=Inscrição para Newsletter&body=Nome: ${name}%0D%0AEmail: ${email}`;
   });
-  
+});
