969e38848398f97a4412e94dee7a491c4d24b7a9
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-12-15 18:09:47 -0300

    151201231

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelectorAll(".carousel-item");
   const totalSlides = slides.length;
 
-  document.querySelector(".carousel-control-prev").addEventListener("click", function () {
-    goToSlide(currentSlide - 1);
-  });
+  document
+    .querySelector(".carousel-control-prev")
+    .addEventListener("click", function () {
+      goToSlide(currentSlide - 1);
+    });
 
-  document.querySelector(".carousel-control-next").addEventListener("click", function () {
-    goToSlide(currentSlide + 1);
-  });
+  document
+    .querySelector(".carousel-control-next")
+    .addEventListener("click", function () {
+      goToSlide(currentSlide + 1);
+    });
 
   function goToSlide(n) {
     slides[currentSlide].classList.remove("active");
     currentSlide = (n + totalSlides) % totalSlides;
     slides[currentSlide].classList.add("active");
-    document.querySelector(".carousel-inner").style.transform = `translateX(-${currentSlide * 100}%)`;
+    document.querySelector(".carousel-inner").style.transform = `translateX(-${
+      currentSlide * 100
+    }%)`;
   }
 });
